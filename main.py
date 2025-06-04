a6e29c1df9bcc2a80e4cd6e4a350cb07af922103
Pavel <163pashka163@mail.ru>
2025-06-04 21:28:10 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,87 +1,36 @@
 # Задача 1
 
-# with open('input.txt') as f_in, open('output.txt', 'w') as f_out:
-#     text = f_in.read()
-#     long_words = [word for word in text.split() if len(word) >= 7]
-#     f_out.write(' '.join(long_words))
+# class Person:
+#     name = "Vasya"
+#
+# id_1 = Person()
+# print(id_1.name)
 
 # Задача 2
 
-# with open('input.txt', 'r', encoding='utf-8') as source:
-#     with open('output.txt', 'w', encoding='utf-8') as target:
-#         for line in source:
-#             target.write(line)
+# class Person:
+#     name = "Vasya"
+#
+# id_1 = Person()
+# id_1.name = "Lena"
+# Person.name = "Masha"
+#
+# print(Person.name)
+# print(id_1.name)
 
 # Задача 3
 
-# with open('input.txt', 'r', encoding='utf-8') as f:
-#     lines = f.readlines()
-# with open('output.txt', 'w', encoding='utf-8') as f:
-#     f.writelines(reversed(lines))
+class Holiday:
+    pass
 
-# Задача 4
+friends = Holiday()
+friends.name1 = 'Sveta'
+friends.name2 = 'Katya'
+friends.name3 = 'Lena'
+friends.name4 = 'Natasha'
+friends.name5 = 'Leonardo DiCaprio'
 
-# with open('input.txt', 'r') as f:
-#     lines = f.readlines()
-#
-# last_no_comma = -1
-# for i, line in enumerate(lines):
-#     if ',' not in line:
-#         last_no_comma = i
-#
-# pos = last_no_comma + 1 if last_no_comma != -1 else len(lines)
-#
-# lines.insert(pos, '************\n')
-#
-# with open('output.txt', 'w') as f:
-#     f.writelines(lines)
+friends.name5 = 'Pavel'
 
-# Задача 5
-#
-# char = input("Введите символ: ").lower()
-#
-# count = 0
-# with open('файла.txt', 'r', encoding='utf-8') as file:
-#     for line in file:
-#         words = line.split()
-#         for word in words:
-#             if word.lower().startswith(char):
-#                 count += 1
-#
-# print(f"Количество слов {count}")
-
-# Задача 6
-
-# with open('input.txt', 'r') as f_in, open('output.txt', 'w') as f_out:
-#     for line in f_in:
-#         new_line = line.replace('*', 'temp').replace('&', '*').replace('temp', '&')
-#         f_out.write(new_line)
-
-# Задача 7
-
-# lines = ["Первая строка", "Вторая строка", "Третья строка"]
-#
-# with open('output.txt', 'w', encoding='utf-8') as file:
-#     for line in lines:
-#         file.write(line + '\n')
-
-# Задача 8
-
-# lines = ["Первая строка", "Вторая строка", "Третья строка"]
-#
-# with open('output.txt', 'w', encoding='utf-8') as file:
-#     file.write('\n'.join(lines))
-
-# Задача 9
-
-# with open('файл.txt', 'r', encoding='utf-8') as file:
-#     content = file.read()
-#     sumbl = len(content)
-#     print(f"Количество символов: {sumbl}")
-
-# Задача 10
-
-# with open('файл.txt', 'r', encoding='utf-8') as file:
-#     lines = file.readlines()
-#     num_lines = len(lines)
-#     print(f"Количество строк в файле: {num_lines}")
+print("\nОбновленный список друзей:")
+print(friends.name1, friends.name2, friends.name3, friends.name4, friends.name5)
